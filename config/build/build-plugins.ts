cb2fc3b48a71c518fdbf005ecc0bf70e9bbbe7af
ilya-tulin <tulin.ilya@bk.ru>
2023-03-30 00:10:29 +0300

    feat(IT-15): некоторые правки

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -8,18 +8,29 @@ import { TBuildOptions } from "./typings";
 export const buildPlugins = ({
     paths,
     isDev,
-}: TBuildOptions): webpack.WebpackPluginInstance[] => [
-    new HTMLWebpackPlugin({
-        template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: "css/[name].[contenthash:8].css",
-        chunkFilename: "css/[name].[contenthash:8].css",
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin(),
-];
+}: TBuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV: JSON.stringify(isDev),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin(),
+    ];
+
+    if (isDev) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+            new BundleAnalyzerPlugin()
+        );
+    }
+
+    return plugins;
+};
