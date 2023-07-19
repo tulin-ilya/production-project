61cb214b42e972f19dea36abac3bf3cbe0fb6f99
ilya-tulin <tulin.ilya@bk.ru>
2023-07-19 23:04:22 +0300

    feat(IT-20): ui-тесты и некоторые правки

--- src/f_entities/user/stories/user.stories.tsx
+++ src/f_entities/user/stories/user.stories.tsx
@@ -1,17 +1,18 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+/* eslint-disable storybook/default-exports */
+// import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import { User as UserComponent } from "../index";
-import { ARGS, ARGS_TYPES } from "./config";
+// import { User as UserComponent } from "../index";
+// import { ARGS, ARGS_TYPES } from "./config";
 
-export default {
-    title: "Entities/User",
-    component: UserComponent,
-    args: ARGS,
-    argTypes: ARGS_TYPES,
-} as ComponentMeta<typeof UserComponent>;
+// export default {
+//     title: "Entities/User",
+//     component: UserComponent,
+//     args: ARGS,
+//     argTypes: ARGS_TYPES,
+// } as ComponentMeta<typeof UserComponent>;
 
-const Template: ComponentStory<typeof UserComponent> = (args) => (
-    <UserComponent {...args} />
-);
+// const Template: ComponentStory<typeof UserComponent> = (args) => (
+//     <UserComponent {...args} />
+// );
 
-export const User = Template.bind({});
+// export const User = Template.bind({});
