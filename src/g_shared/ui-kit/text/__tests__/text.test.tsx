8bff446eb55482506bfeb55cfebdd55c4ee34c00
ilya-tulin <tulin.ilya@bk.ru>
2023-07-21 21:05:26 +0300

    feat(IT-22): почищены ворнинги eslint

--- src/g_shared/ui-kit/text/__tests__/text.test.tsx
+++ src/g_shared/ui-kit/text/__tests__/text.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
+// import { render, screen } from "@testing-library/react";
 
-import { Text } from "../index";
+// import { Text } from "../index";
 
 describe("Text", () => {
     it("Test", () => {
