cfde5699e054aea0425ebd945aec458b7eb12616
ilya-tulin <tulin.ilya@bk.ru>
2023-07-21 14:41:57 +0300

    feat(IT-22): расширение глобальных стилей и конфига компонентов

--- src/g_shared/configs/component-base-type.ts
+++ src/g_shared/configs/component-base-type.ts
@@ -1,15 +1,19 @@
 import {
     CLEAR,
+    DANGER,
     NORMAL,
     PRIMARY,
     SECONDARY,
+    WARNING,
 } from "@shared/configs/components-view";
 
 export type TView =
     | typeof CLEAR
+    | typeof DANGER
     | typeof NORMAL
     | typeof PRIMARY
-    | typeof SECONDARY;
+    | typeof SECONDARY
+    | typeof WARNING;
 
 export type TComponentBaseProps = {
     /** Для margin и padding */
--- src/g_shared/configs/components-view.ts
+++ src/g_shared/configs/components-view.ts
@@ -2,3 +2,5 @@ export const CLEAR = "clear";
 export const NORMAL = "normal";
 export const PRIMARY = "primary";
 export const SECONDARY = "secondary";
+export const DANGER = "danger";
+export const WARNING = "warning";
