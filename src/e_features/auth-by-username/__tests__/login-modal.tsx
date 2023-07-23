a4935a8b5a381b0ef54ce40cd49eca990db3dede
tulin-ilya <72538725+tulin-ilya@users.noreply.github.com>
2023-07-23 20:24:30 +0300

    feat(IT-24): Тестирование фичи authByUsername. TestAsyncThunk (#10)
    * feat(IT-24): правки БД
    
    * feat(IT-24): Тесты для AuthByUsername

--- src/e_features/auth-by-username/__tests__/login-modal.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from "@testing-library/react";
-
-// import { AuthByUsername } from "../index";
-
-describe("AuthByUsername", () => {
-    it("Test", () => {
-        expect("test").toEqual("test");
-    });
-});
---
+++ src/e_features/auth-by-username/__tests__/services.test.ts
@@ -0,0 +1,47 @@
+import { TStateSchema } from "@app/providers/store-provider";
+import { userActions } from "@entities/user";
+import { Dispatch } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { loginByUsername } from "../src/services/login-by-username";
+
+jest.mock("axios");
+
+const mockedAxios = jest.mocked(axios, true);
+
+describe("loginByUsername", () => {
+    let dispatch: Dispatch;
+    let getState: () => TStateSchema;
+
+    beforeEach(() => {
+        dispatch = jest.fn();
+        getState = jest.fn();
+    });
+    it("Успешная авторизация", async () => {
+        const userValue = { username: "admin", id: "1" };
+        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+
+        const action = loginByUsername({ password: "123", username: "admin" });
+        const result = await action(dispatch, getState, undefined);
+
+        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe("fulfilled");
+        expect(result.payload).toEqual(userValue);
+        expect(dispatch).toHaveBeenCalledTimes(3);
+        expect(dispatch).toHaveBeenCalledWith(
+            userActions.setAuthData(userValue)
+        );
+    });
+
+    it("Сервер прислал ошибку", async () => {
+        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const action = loginByUsername({ password: "123", username: "admin" });
+        const result = await action(dispatch, getState, undefined);
+
+        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe("rejected");
+        expect(result.payload).toEqual("error");
+        expect(dispatch).toHaveBeenCalledTimes(2);
+    });
+});
