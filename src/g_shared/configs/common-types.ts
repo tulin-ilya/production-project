2075f93f96bb9f0295049a464943153aa92658d8
ilya-tulin <tulin.ilya@bk.ru>
2023-07-24 19:18:34 +0300

    feat(IT-25): дополнение типов

---
+++ src/g_shared/configs/common-types.ts
@@ -0,0 +1,12 @@
+export type TCurrency = "RUB" | "EUR" | "USD";
+
+export type TCountry =
+    | "Russia"
+    | "Ukraine"
+    | "USA"
+    | "Belarus"
+    | "Kazakhstan"
+    | "Armenia"
+    | "Georgia"
+    | "Great Britain"
+    | "German";
