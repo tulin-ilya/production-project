8ff1bdb2ce93b5ea4a0d4f1e19c3684718a8df42
ilya-tulin <tulin.ilya@bk.ru>
2023-07-24 19:19:28 +0300

    feat(IT-25): дополнение путей

--- src/a_app/providers/app-router/routes.tsx
+++ src/a_app/providers/app-router/routes.tsx
@@ -1,7 +1,8 @@
 import { AboutPage } from "@pages/about-page";
 import { MainPage } from "@pages/main-page";
 import { NotFoundPage } from "@pages/not-found-page";
-import { ABOUT, HOME, MAIN } from "@shared/configs/routes-config";
+import { ProfilePage } from "@pages/profile-page";
+import { ABOUT, HOME, MAIN, PROFILE } from "@shared/configs/routes-config";
 import type { RouteProps } from "react-router-dom";
 
 export const routes: RouteProps[] = [
@@ -21,6 +22,11 @@ export const routes: RouteProps[] = [
         path: `/${ABOUT}`,
         element: <AboutPage />,
     },
+    {
+        path: `/${PROFILE}`,
+        element: <ProfilePage />,
+    },
+    // last
     {
         path: "*",
         element: <NotFoundPage />,
