ab3589535724f93b52b2f13e72e125a22f53e846
ilya-tulin <tulin.ilya@bk.ru>
2023-07-31 23:37:26 +0300

    feat(IT-29): на страницу добавлены новые виджеты

--- src/c_pages/profile-page/src/profile-page.tsx
+++ src/c_pages/profile-page/src/profile-page.tsx
@@ -1,12 +1,21 @@
 import type { TAppDispatch } from "@app/providers/store-provider";
-import { fetchProfileData, Profile, profileReducer } from "@entities/profile";
+import {
+    fetchProfileData,
+    profileActions,
+    profileReducer,
+} from "@entities/profile";
+import { getProfileState } from "@entities/profile";
 import type { TReducersList } from "@shared/ui-kit/dynamic-module-loader";
 import { DynamicModuleLoader } from "@shared/ui-kit/dynamic-module-loader";
-import { memo, useEffect, useMemo } from "react";
-import { useDispatch } from "react-redux";
+import { ProfileCard, TProfileFieldName } from "@widgets/profile-card";
+import { ProfileCardHeader } from "@widgets/profile-card-header";
+import { memo, useCallback, useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProfilePage = memo(() => {
     const dispatch = useDispatch<TAppDispatch>();
+    const { isLoading, currentData, error, readonly } =
+        useSelector(getProfileState);
 
     useEffect(() => {
         dispatch(fetchProfileData());
@@ -19,9 +28,23 @@ const ProfilePage = memo(() => {
         []
     );
 
+    const onChangeProfile = useCallback(
+        (fieldName: TProfileFieldName, value?: string) => {
+            dispatch(profileActions.updateProfile({ [fieldName]: value }));
+        },
+        [dispatch]
+    );
+
     return (
         <DynamicModuleLoader reducers={reducers} removeReducerAfterUnmount>
-            <Profile />
+            <ProfileCardHeader />
+            <ProfileCard
+                isLoading={isLoading}
+                readonly={readonly}
+                data={currentData}
+                error={error}
+                onChange={onChangeProfile}
+            />
         </DynamicModuleLoader>
     );
 });
